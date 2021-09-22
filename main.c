c27dffc80105d079d6e9d9e8e87eb507cb3f6760
Brian Starkey <stark3y@gmail.com>
2021-09-22 22:19:10 +0100

    Cut down and copy_to_ram
    Paradoxically, linking with just `pico_stdlib` results in a much smaller
    binary (~9kB) than not doing so (~17kB). Presumably this is due to
    stdlib making more use of bootrom?

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+#include "pico/time.h"
+#include "hardware/gpio.h"
 
 void main(void)
 {
-	stdio_init_all();
-
+	gpio_init(PICO_DEFAULT_LED_PIN);
+	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 	while (1) {
-		printf("Hello, World!\n");
-		sleep_ms(1000);
+		gpio_put(PICO_DEFAULT_LED_PIN, 1);
+		sleep_ms(500);
+		gpio_put(PICO_DEFAULT_LED_PIN, 0);
+		sleep_ms(500);
 	}
 }
